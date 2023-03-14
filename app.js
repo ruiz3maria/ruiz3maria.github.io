d0fc8e16f91abe0ac9d17ae51a6a5a845d36039f
ruiz3maria <126509948+ruiz3maria@users.noreply.github.com>
2023-03-14 15:14:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ function mostrarMenu(){
         menu_visible = false;
     }
 }
+let links = document.querySelectorAll("nav a");
+for(var x = 0; x <links.length;x++){
+    links[x].onclick = function(){
+        menu.style.display = "none";
+        menu_visible = false;
+    }
+}
